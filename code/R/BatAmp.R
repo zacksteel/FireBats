7e49f0b6447dad38613d9c8f0c3b171fa4639a8a
Zack Steel <zacksteel@Incendio.local>
2020-04-15 14:33:47 -0700

    individual years datapreppred

--- code/R/BatAmp.R
+++ code/R/BatAmp.R
@@ -4,4 +4,35 @@
 library(tidyverse)
 
 ## Bring in count data
-d <- read.csv("data/counts.csv")
+d <- read.csv("data/counts.csv") %>% 
+  ## create some columns that match batamp template
+  mutate(first_name = "Zack",
+         last_name = "Steel",
+         det_mfg = "SonoBat",
+         det_model = "SM3BAT",
+         ## Specify mic type
+         mic_type = case_when(sm3_u1 == 0 ~ "SMM-U1",
+                              sm3_u1 == 1 ~ "SM3-U1"),
+         refl_type = "None",
+         mic_ht_units = "meters",
+         call_id_1 = "Sonobat 3",
+         call_id_2 = NA,
+         site_ID = forest,
+         det_id = point,
+         species = toupper(species)
+         ) %>% 
+  ## remove some columns
+  select(-point, -sm3_u1, -forest)
+
+## make it wide
+d_w <- pivot_wider(d, names_from = species, values_from = ccounts) %>% 
+  ##reorder columns
+  select(first_name, last_name, y_coord, x_coord, det_mfg, det_model, mic_type, refl_type,
+         mic_ht, mic_ht_units, everything())
+
+## data must be within unique years
+for(i in unique(d$year)) {
+  sub <- filter(d_w, year == i) %>% 
+    select(-year)
+  write.csv(sub, paste0("data/batamp/zls_", i, ".csv"), row.names = F)
+}
